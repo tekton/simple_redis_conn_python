af4d9460738fae12822e1e9a47537018af1dadc6
Tyler Agee <tyler@pyroturtle.com>
2016-03-26 15:39:20 -0700

    skeleton for editing

---
+++ simple_redis_conn/__init__.py
@@ -0,0 +1,4 @@
+from .redis_conn import r
+from .redis_conn import w
+
+__all__ = ["r", "w"]
---
+++ simple_redis_conn/redis_conn.py
@@ -0,0 +1,22 @@
+import os
+import redis
+
+
+RC_DB_TYPE = os.getenv("RC_DB_TYPE", "strict")
+
+if RC_DB_TYPE == "strict":
+    READ_HOST = os.getenv("READ_HOST", "localhost")
+    READ_PORT = int(os.getenv("READ_PORT", "6379"))
+    READ_DB = int(os.getenv("READ_DB", "0"))
+    WRITE_HOST = os.getenv("WRITE_HOST", "localhost")
+    WRITE_PORT = int(os.getenv("WRITE_PORT", "6379"))
+    WRITE_DB = int(os.getenv("WRITE_DB", "0"))
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    w = redis.StrictRedis(host='localhost', port=6379, db=0)
+elif RC_DB_TYPE == "url":
+    REDIS_READ_URL = os.getenv("REDIS_READ_URL", "redis://localhost")
+    REDIS_WRITE_URL = os.getenv("REDIS_WRITE_URL", "redis://localhost")
+    r = redis.from_url(REDIS_URL)
+    w = redis.from_url(REDIS_URL)
+else:
+    raise
